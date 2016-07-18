535f79bd243e815c6700b3cbf3f554a4c10ac200
B. Morgan Murrah <beau@airbridge.ac.nz>
2016-07-17 23:02:38 -0400

    End of Exercise 9

--- main.js
+++ main.js
@@ -2,3 +2,26 @@ var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
+
+if (cardOne == cardTwo) {
+  alert("You found a match!")
+} 
+if (cardThree == cardFour) {
+  alert("You found a match!")
+} 
+if (cardTwo != cardFour) {
+  alert("No luck, try again!")
+};
+
+
+// if ( king && king ) { alert("Match!"); 
+// if else ( queen & queen ) { alert("Match!"); 
+// else ( kind && queen ) { alert("Sorry, try again!");}
+
+
+
+// for (initialization; condition; finalExpression) {
+//   // A block of code.
+// }
+// Loop Basics ^^^
+
